5fd5aa0702f866d2be8e2719b86d26fe7c98bef6
AxMari <AlexisMarien@gmail.com>
2016-04-25 10:10:34 -0400

    updated for err code

--- temp.js
+++ temp.js
@@ -3,12 +3,16 @@ button.addEventListener('click', function() {
 	var Usertemp = parseInt(document.getElementById('userTemp').value);
 	var unit = document.getElementById('mySelect').value;	
 	var answer = document.getElementById('answer');
-	if(unit === "C") {
-		newTemp = ((Usertemp * 9/5) + 32 + "\xB0F");
-	}
-	else if(unit === "F") {
-		newTemp = ((Usertemp - 32) * (5/9) + "\xB0C");
-	}
+		if(!isNaN(Usertemp)){
+			if(unit === "C") {
+				newTemp = ((Usertemp * 9/5) + 32 + "\xB0F");
+			}
+			else if(unit === "F") {
+				newTemp = ((Usertemp - 32) * (5/9) + "\xB0C");
+			}
+		}else{
+			newTemp = "Err: Not a number";
+		}
 	answer.innerText = newTemp;	
 });
 
